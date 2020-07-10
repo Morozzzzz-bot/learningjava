efb61101b331c41f149d1f5f1fdfad81a2dba8a1
Selesito <selesitomorozov@gmail.com>
2020-07-10 22:48:11 +0400

    0. Что такое тестирование.

--- chapter_001/src/main/java/converter/Converter.java
+++ chapter_001/src/main/java/converter/Converter.java
@@ -13,9 +13,16 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        int euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        int dollar = Converter.rubleToDollar(240);
-        System.out.println("240 rubles are " + dollar + " dollar.");
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        int inD = 240;
+        int expectedD = 4;
+        int outD = rubleToDollar(inD);
+        boolean passedD = expectedD == outD;
+        System.out.println("240 rubles are 4. Test result : " + passedD);
     }
+
 }
