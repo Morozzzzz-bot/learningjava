39ca43a84db228a19916d9aa0aa27415577f19cf
Selesito <selesitomorozov@gmail.com>
2020-07-16 00:52:24 +0400

    Философия Java. Всё является объектом. (9)

---
+++ chapter_001/src/main/java/everything/AutoBox.java
@@ -0,0 +1,19 @@
+package everything;
+
+public class AutoBox {
+    public static void main(String[] args) {
+        boolean b = false;
+        Boolean B = b;
+        System.out.println(b);
+        System.out.println(B);
+        char x = 'x';
+        Character ch = x;
+        System.out.println(x);
+        System.out.println(ch);
+        double a = 3.14;
+        Double A = a;
+        System.out.println(a);
+        System.out.println(A);
+    }
+
+}
