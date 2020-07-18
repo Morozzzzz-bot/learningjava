8416741e7d9612094ea8cf9e18119881b2070c55
Selesito <selesitomorozov@gmail.com>
2020-07-18 22:43:33 +0400

    Настройка Maven

--- chapter_001/src/main/java/everything/AutoBox.java
+++ chapter_001/src/main/java/everything/AutoBox.java
@@ -3,17 +3,17 @@ package everything;
 public class AutoBox {
     public static void main(String[] args) {
         boolean b = false;
-        Boolean B = b;
+        Boolean bB = b;
         System.out.println(b);
-        System.out.println(B);
+        System.out.println(bB);
         char x = 'x';
         Character ch = x;
         System.out.println(x);
         System.out.println(ch);
         double a = 3.14;
-        Double A = a;
+        Double aA = a;
         System.out.println(a);
-        System.out.println(A);
+        System.out.println(aA);
     }
 
 }
