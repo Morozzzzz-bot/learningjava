839ae9d1e719f2935d8087d7b883d96ab0fd467b
Selesito <selesitomorozov@gmail.com>
2020-07-22 23:23:16 +0400

    4.3. А существует ли треугольник?

---
+++ chapter_001/src/test/java/condition/TriangleTest.java
@@ -0,0 +1,25 @@
+package condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TriangleTest {
+    @Test
+    public void whenExist() {
+        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenExistTwo() {
+        boolean result = Triangle.exist(6.0, 2.0, 3.0);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenExistwoT() {
+        boolean result = Triangle.exist(1.0, 2.0, 3.0);
+        assertThat(result, is(false));
+    }
+}
