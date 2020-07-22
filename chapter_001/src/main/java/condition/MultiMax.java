b4cacf1eb2c101bdc44d266748b39a16406aebcc
Selesito <selesitomorozov@gmail.com>
2020-07-22 23:38:57 +0400

    4.4. Максимум из трех чисел

---
+++ chapter_001/src/main/java/condition/MultiMax.java
@@ -0,0 +1,9 @@
+package condition;
+
+public class MultiMax {
+    public static int max(int first, int second, int third) {
+        int resultOne = first > second ? first : second;
+        int resultTwo = resultOne > third ? resultOne : third;
+        return resultTwo;
+    }
+}
