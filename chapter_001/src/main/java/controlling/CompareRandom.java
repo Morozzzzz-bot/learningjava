d34e23cf229337d83b1f03af9451769d904dc62e
Selesito <selesitomorozov@gmail.com>
2020-07-25 22:38:03 +0400

    Философия Java. 4.Управляющие конструкции (3)

--- chapter_001/src/main/java/controlling/CompareRandom.java
+++ chapter_001/src/main/java/controlling/CompareRandom.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class CompareRandom {
     public static void main(String[] args) {
-        Random rand1 = new Random(1);
-        Random rand2 = new Random(1);
-        for (int i = 0; i < 25; i++) {
+        Random rand1 = new Random();
+        Random rand2 = new Random();
+        while (true) {
             int countOne = rand1.nextInt();
             int countTwo = rand2.nextInt();
             if (countOne < countTwo) {
