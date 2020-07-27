adb093b9101cafa30ed091c66b948f328fa24779
Selesito <selesitomorozov@gmail.com>
2020-07-27 22:00:22 +0400

    Философия Java. 4.Управляющие конструкции (7)

--- chapter_001/src/main/java/controlling/Count.java
+++ chapter_001/src/main/java/controlling/Count.java
@@ -3,6 +3,12 @@ package controlling;
 public class Count {
     public static void main(String[] args) {
         for (int i = 1; i < 101; i++) {
+            if (i == 99) {
+                break;
+            }
+            if (i == 98) {
+                return;
+            }
             System.out.println(i);
         }
     }
