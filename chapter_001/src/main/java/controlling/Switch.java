65597baef09fe52415024c2766421107bd37a8fd
Selesito <selesitomorozov@gmail.com>
2020-07-27 22:23:40 +0400

    Философия Java. 4.Управляющие конструкции (8)

---
+++ chapter_001/src/main/java/controlling/Switch.java
@@ -0,0 +1,36 @@
+package controlling;
+
+public class Switch {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            switch (i) {
+                case 1:
+                    System.out.println("Число " + i);
+                    break;
+                case 2:
+                    System.out.println("Число " + i);
+                    break;
+                case 3:
+                    System.out.println("Число " + i);
+                    break;
+                case 4:
+                    System.out.println("Число " + i);
+                    break;
+                case 5:
+                    System.out.println("Число " + i);
+                    break;
+                case 6:
+                    System.out.println("Число " + i);
+                    break;
+                case 7:
+                    System.out.println("Число " + i);
+                    break;
+                case 8:
+                    System.out.println("Число " + i);
+                    break;
+                default: System.out.println("Число больше 8");
+            }
+
+        }
+    }
+}
