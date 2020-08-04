139a0ccf5bfc97a2162c3bab3a92a9fa2b66deb2
Selesito <selesitomorozov@gmail.com>
2020-08-04 17:45:14 +0400

    6.4.1. Слово заканчивается с ...

---
+++ chapter_001/src/test/java/array/EndsWithTest.java
@@ -0,0 +1,33 @@
+package array;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class EndsWithTest {
+
+    @Test
+    public void whenEndWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotEndWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'a'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse2() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'l', 'a'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
+}
