567cb89d4daf69fb6c9766e313929bb8aff86828
Selesito <selesitomorozov@gmail.com>
2020-08-10 17:09:11 +0400

    2. Чтение кода и базовые ошибки.

--- chapter_001/src/main/java/calculator/CalculatorNumber.java
+++ chapter_001/src/main/java/calculator/CalculatorNumber.java
@@ -1,16 +1,16 @@
 package calculator;
 
-public class Calculator {
+public class CalculatorNumber {
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
+        int numberOne = 1;
+        int numberTwo = 2;
         int six = 6;
         int four = 4;
         int five = 5;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        int onePlusTwo = one + two;
+        int sixDivTwo = six / numberTwo;
+        int fiveMinusTwo = five - numberTwo;
+        int fourTimeTwo = four * numberTwo;
+        int onePlusTwo = numberOne + numberTwo;
         System.out.println(onePlusTwo);
         System.out.println(sixDivTwo);
         System.out.println(fourTimeTwo);
