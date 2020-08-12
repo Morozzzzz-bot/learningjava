5c1ae959de367d1c68afaf4afdd8ab0189e74e8a
Selesito <selesitomorozov@gmail.com>
2020-08-12 23:09:09 +0400

    Рефакторинг 2.0

--- chapter_001/src/test/java/condition/PointTest.java
+++ chapter_001/src/test/java/condition/PointTest.java
@@ -8,21 +8,27 @@ import org.junit.Test;
 public class PointTest {
     @Test
     public void distancePointTest() {
-        double out = Point.distance(0, 0, 2, 0);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double out = a.distance(b);
         double expected = 2.0;
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void distancePointTestTwo() {
-        double out = Point.distance(2, 4, 4, 8);
+        Point a = new Point(2, 4);
+        Point b = new Point(4, 8);
+        double out = a.distance(b);
         double expected = 4.47213595499958;
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void distancePointTestThird() {
-        double out = Point.distance(2, 2, 2, 2);
+        Point a = new Point(2, 2);
+        Point b = new Point(2, 2);
+        double out = a.distance(b);
         double expected = 0.0;
         Assert.assertEquals(expected, out, 0.01);
     }
