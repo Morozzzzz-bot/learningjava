67fedb07a505f1868c3a445b6502a65cf50476f5
Selesito <selesitomorozov@gmail.com>
2020-08-12 22:48:20 +0400

    1.5. Поля объекта

--- chapter_002/src/main/java/oop/Cat.java
+++ chapter_002/src/main/java/oop/Cat.java
@@ -1,6 +1,18 @@
 package oop;
 
 public class Cat {
+    private String food;
+    private String name;
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+    public void eat(String meat) {
+        this.food = meat;
+    }
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
     public String sound() {
         String voice = "may-may";
         return voice;
@@ -9,5 +21,15 @@ public class Cat {
         Cat peppy = new Cat();
         String say = peppy.sound();
         System.out.println("Peppy says " + say);
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Котёнок Гав");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Котёнок Чёрный");
+        black.eat("fish");
+        black.show();
     }
 }
