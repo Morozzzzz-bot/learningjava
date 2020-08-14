a8b279cf2447ed440a5f32929342981a033a2cd0
Selesito <selesitomorozov@gmail.com>
2020-08-14 22:09:37 +0400

    3. Перегрузить метод max для трех чисел

--- chapter_001/src/test/java/condition/MaxTest.java
+++ chapter_001/src/test/java/condition/MaxTest.java
@@ -12,19 +12,19 @@ public class MaxTest {
 
     @Test
     public void whenMax4To2Then4() {
-        int result = Max.max(4, 2);
+        int result = Max.max(4, 2, 3);
         assertThat(result, is(4));
     }
 
     @Test
     public void whenMax5To7Then7() {
-        int result = Max.max(5, 7);
-        assertThat(result, is(7));
+        int result = Max.max(5, 7, 9,10);
+        assertThat(result, is(10));
     }
 
     @Test
     public void whenMax2To2Then2() {
-        int result = Max.max(2, 2);
-        assertThat(result, is(2));
+        int result = Max.max(2, 2,5,7);
+        assertThat(result, is(7));
     }
 }
