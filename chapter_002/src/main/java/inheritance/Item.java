c60b033d7d6fa9dde59c8515572ca7858f0c2aae
Selesito <selesitomorozov@gmail.com>
2020-08-15 11:44:35 +0400

    2. Перегрузка конструктора.

---
+++ chapter_002/src/main/java/inheritance/Item.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class Item {
+
+    public Item() {
+    }
+
+    public Item(String name) {
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        Item item1 = new Item("Hello");
+    }
+}
---
+++ chapter_002/src/main/java/tracker/Item.java
@@ -0,0 +1,22 @@
+package tracker;
+
+public class Item {
+    private String name;
+    private int id;
+
+    public Item() {
+    }
+    public Item(int id) {
+        this.id = id;
+    }
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        Item item1 = new Item(56);
+        Item item2 = new Item(56, "Job");
+    }
+}
