aeefa5a6dd562684c1299cc6c233bbf2f5266cba
Selesito <selesitomorozov@gmail.com>
2020-08-19 22:48:45 +0400

    Java. Библиотека профессионала. Кей Хорстманн 222 стр

---
+++ chapter_002/src/main/java/equals/Employee.java
@@ -0,0 +1,32 @@
+package equals;
+
+import java.time.LocalDate;
+
+public class Employee {
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee(
+    String name, double salary, int year, int month, int day) {
+        this.name = name;
+        this.salary = salary;
+        hireDay = LocalDate.of(year, month, day);
+    }
+    public String getNameO() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireDayO() {
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent) {
+       double raise = salary * byPercent / 100;
+       salary += raise;
+    }
+}
