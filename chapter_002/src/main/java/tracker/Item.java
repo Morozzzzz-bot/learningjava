42879751aeeca445244f8636ba09b07ac73649a7
Selesito <selesitomorozov@gmail.com>
2020-08-19 21:47:44 +0400

    5. Реализовать класс Tracker

--- chapter_002/src/main/java/tracker/Item.java
+++ chapter_002/src/main/java/tracker/Item.java
@@ -14,6 +14,22 @@ public class Item {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public static void main(String[] args) {
         Item item = new Item();
         Item item1 = new Item(56);
--- chapter_002/src/main/java/tracker/Tracker.java
+++ chapter_002/src/main/java/tracker/Tracker.java
@@ -1,4 +1,56 @@
 package tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item findById(int id) {
+        Item rsl = null;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getId() == id) {
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        int value = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item tmp = items[index];
+            if (tmp != null) {
+                rsl[value] = tmp;
+                value++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, value);
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int value = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item name = items[index];
+            if (key.equals(name)) {
+                rsl[value] = name;
+                value++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, value);
+        return rsl;
+    }
+
 }
