51e3023519069f1b18ada3e8e49ff7ebfb3255ad
Selesito <selesitomorozov@gmail.com>
2020-08-26 18:33:27 +0400

    3.1.2 Приведение типов. Повышение типа и понижение типа.

---
+++ chapter_002/src/main/java/oop/CollegeTwo.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class CollegeTwo {
+    public static void main(String[] args) {
+        Freshman man = new Freshman();
+        StudentTwo manTwo = man;
+        Object manThird = man;
+    }
+}
